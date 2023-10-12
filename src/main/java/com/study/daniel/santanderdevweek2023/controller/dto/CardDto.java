e1f510d74014266d0cc4c109e4a131fe0622ba93
daniellimadev <pdaniel643@gmail.com>
2023-10-12 13:32:55 -0300

    finalizing the Dto
    Signed-off-by: daniellimadev <pdaniel643@gmail.com>

---
+++ src/main/java/com/study/daniel/santanderdevweek2023/controller/dto/CardDto.java
@@ -0,0 +1,21 @@
+package com.study.daniel.santanderdevweek2023.controller.dto;
+
+import com.study.daniel.santanderdevweek2023.domain.model.Card;
+
+import java.math.BigDecimal;
+
+public record CardDto(Long id, String number, BigDecimal limit) {
+
+    public CardDto(Card model) {
+        this(model.getId(), model.getNumber(), model.getLimit());
+    }
+
+    public Card toModel() {
+        Card model = new Card();
+        model.setId(this.id);
+        model.setNumber(this.number);
+        model.setLimit(this.limit);
+        return model;
+    }
+
+}
---
+++ src/main/java/com/study/daniel/santanderdevweek2023/controller/dto/FeatureDto.java
@@ -0,0 +1,18 @@
+package com.study.daniel.santanderdevweek2023.controller.dto;
+
+import com.study.daniel.santanderdevweek2023.domain.model.Feature;
+
+public record FeatureDto(Long id, String icon, String description) {
+
+    public FeatureDto(Feature model) {
+        this(model.getId(), model.getIcon(), model.getDescription());
+    }
+
+    public Feature toModel() {
+        Feature model = new Feature();
+        model.setId(this.id);
+        model.setIcon(this.icon);
+        model.setDescription(this.description);
+        return model;
+    }
+}
